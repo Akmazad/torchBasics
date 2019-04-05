36eb3d0620447f6374b3d6eba0663ceee1d40f05
Akmazad <a.azad@monash.edu>
2019-04-05 16:16:02 +1100

    Update tensorBinarization.py

--- tensorBinarization.py
+++ tensorBinarization.py
@@ -4,8 +4,8 @@ a = np.array([0.1, 0.4, 0.35, 0.8])
 a = np.array([a,a])
 prob = torch.from_numpy(a)  # convert numpy to torch tensor
 
-t0 = torch.zeros(prob.shape) # create a tensor of all zeros with shape as 'prob'
-t1 = torch.ones(prob.shape) # create a tensor of all ones with shape as 'prob'
+t0 = torch.zeros(prob.shape) # create a tensor of all zeros with shape as 'prob': another approach: t0 = torch.zeros_like(prob)
+t1 = torch.ones(prob.shape) # create a tensor of all ones with shape as 'prob' : : another approach: t1 = torch.ones_like(prob)
 th = 0.4
 out = torch.where(prob >= th, t1, t0) # binarize based on the condition
 print(out) # print binarized Tensor
